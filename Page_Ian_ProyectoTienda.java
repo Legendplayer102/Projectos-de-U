054fe3d1e2d857cbf7698bc5f0bf60e94b65d96e
IP <iankpage@gmail.com>
2025-05-01 16:01:41 -0600

    Add files via upload

---
+++ Page_Ian_ProyectoTienda.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package page_ian_proyectotienda;
+
+/**
+ *
+ * @author iankp
+ */
+public class Page_Ian_ProyectoTienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
